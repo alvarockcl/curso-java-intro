18499fd27dcdedef2e0af926e0a953df15d93e3f
Alvaro Fuentes <alvarofue@gmail.com>
2018-07-02 20:39:37 -0400

    Agrega Variables

---
+++ 01 - Introducción a Java/03-Variables/src/main/java/cl/fuentes/app/App.java
@@ -0,0 +1,40 @@
+package cl.fuentes.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Declarar una variable
+
+        int a;
+        String b;
+        char c;
+
+        // Inicializar una variable
+
+        a = 10;
+        b = "Alvaro";
+        c = 'a';
+
+        // Declarar e inicializar variable
+
+        int x = 20;
+        String y = "Fuentes";
+        char z = 'b';
+
+        // Declarar constantes
+
+        final int HORAS_DIA = 20;
+
+        // Imprimir por consola
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(HORAS_DIA);
+
+    }
+}
