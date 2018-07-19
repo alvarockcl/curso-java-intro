3f55efaf8d7048e0f0fb9b62d0184e58e76d62b1
Alvaro Fuentes <alvarofue@gmail.com>
2018-07-19 00:19:42 -0400

    Agrega ejemplo

--- 05 - Elementos Avanzados de Java/01-Exception/src/main/java/cl/fuentes/app/App.java
+++ 05 - Elementos Avanzados de Java/01-Exception/src/main/java/cl/fuentes/app/App.java
@@ -1,10 +1,33 @@
 package cl.fuentes.app;
 
+import java.io.File;
+import java.io.FileReader;
+
+// java -cp target/01-Exception-1.0-SNAPSHOT.jar cl.fuentes.app.App
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Imprimir datos a la consola
-        System.out.println("Hola mundo - bienvenido al curso");
+
+        // Estructura de una excepcion
+        try{
+            
+            //throw new Exception("Error");
+
+            //Integer.parseInt("Valor");
+
+            double div = 10 / 0;
+
+            //File archivo = new File("C://archivo.txt");
+            //FileReader contenidoArchivo = new FileReader(archivo);
+            
+        }catch(Exception e){
+            System.out.println("Bloque que se ejecuta si se produce una excepcion : " + e.getMessage());
+            //System.exit(0);
+        }finally{
+            System.out.println("Bloque que se ejecuta igual si se produce o no una excepcion");
+        }
     }
+
 }
